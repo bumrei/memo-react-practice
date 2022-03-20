f12ef6fe512fad2c00cc5aa7c0642d9133476f08
bumrei <bumrei@gmail.com>
2022-03-21 00:44:09 +0900

    vs code - rsc

---
+++ react-test/src/components/Hahaha.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+// 새로운 프로젝트에서 rsc 누르면 완성 - vs code 플러그인인 Reactjs code snippets의 기능
+
+
+const Hahaha = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Hahaha;
